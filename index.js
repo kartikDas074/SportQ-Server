206cadfdc791ea1ade4d01d389e7b2a3f42a1e92
kartikDas074 <kartikdas209774@gmail.com>
2026-06-07 21:46:22 +0600

    Get All Venue From Database

--- index.js
+++ index.js
@@ -2,15 +2,59 @@ const express=require('express');
 const cors=require('cors');
 const dotenv=require('dotenv');
 const app=express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 dotenv.config();
 
 //port Initialize
 const port=process.env.PORT;
-
+const uri =process.env.MONGODB_URI;
 
 //For cross server
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+  
+    const DB = client.db("SportQuest");
+    const ground = DB.collection("SGround");
+     
+    app.get('/ground',async(req,res)=>{
+        try{
+          const grd=await ground.find({}).toArray();
+          res.status(200).json(grd);
+        }catch(e){
+          res.status(500).json({
+            success:false,
+            msg:e.message
+          })
+        }
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 // Main Code
